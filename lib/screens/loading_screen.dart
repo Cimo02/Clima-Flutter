24fcaebafc5b46e5228dd205cf51e56a046e25c4
Tyler Ciarmataro <tjcimo@gmail.com>
2021-01-16 20:50:59 -0500

    app calls OpenWeather API with user's location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String kApiKey = "970b77d7cfa17e8cd6bedbe89500d128";
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,12 +22,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocation() async {
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    //print(location.latitude);
+    //print(location.longitude);
+  }
+
+  void getData() async {
+    // remember to add https:// or http:// to api calls
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$kApiKey');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var weatherDescription = jsonDecode(data)['weather'][0]['description'];
+      print(weatherDescription);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
+
     return Scaffold();
   }
 }
